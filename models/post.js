65f185dc96c1973caae04506686c61ee5274086f
Proprogam <k.kartal9595@gmail.com>
2023-07-28 23:00:02 +0300

    first commit

--- models/post.js
+++ models/post.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true,
-    trim: true,
-  },
   baslik: {
     type: String,
     required: true,
